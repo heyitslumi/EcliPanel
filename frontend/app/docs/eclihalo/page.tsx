3a933cbc90eb941b3efb75094506129ca83f2ae4
Noname <noname@ecli.app>
2026-07-25 20:56:17 +0000

    Update formating for eclihalo docs > benchmark.

--- frontend/app/docs/eclihalo/page.tsx
+++ frontend/app/docs/eclihalo/page.tsx
@@ -14,8 +14,10 @@ It is designed for high performance and low latency, with a focus on simplicity 
 
 ## Benchmark
 This benchmark compares EcliHalo to Nginx.
-You can view how benchmark works here: https://github.com/thenoname-gurl/EcliPanel/blob/main/halo/bench.sh)
-Anyways here is results, we can see up to 106% improvement in some fields comparing to nginx, higher rps = better.
+
+You can view how benchmark works here: https://github.com/thenoname-gurl/EcliPanel/blob/main/halo/bench.sh
+
+Anyways here are results, we can see up to 106% improvement in some fields comparing to nginx, higher rps = better.
 
 ![Benchmark Results](https://github.com/thenoname-gurl/EcliPanel/blob/main/halo/chart.png?raw=true)
 
